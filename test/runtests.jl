28d2dbb36ebe9de15e4e40df3826555da4d67fa0
Valentina Schüller <valentina.schueller@gmail.com>
2026-06-15 16:58:22 +0200

    Remove project

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using LowRankMethods
-
-B_test = [0 0.5 0 0; -0.5 0 0.5 0; 0 -0.5 0 0.5; 0 0 -1 1]
-@assert time_matrix(4, 1) == B_test
-A_test = [-2.0 1 0 0; 1 -2 1 0; 0 1 -2 1; 0 0 1 -2]
-@assert space_matrix(4, 1) == A_test
-
-@assert eigensolver(20, 20) ≈ solve_sylvester(20, 20)
-@assert eigensolver(20, 20; g=x -> sin(2π * x)) ≈ eigensolver_lr(20, 20; g=x -> sin(2π * x))
